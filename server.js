0244e1193736bc0c7dd6ebd83f3d700104fff49a
chrisfoster76 <chrisfoster186@googlemail.com>
2018-09-26 11:56:46 +0100

    Removed crud

--- server.js
+++ server.js
@@ -1,53 +1,29 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var http = require("http");
-var https = require("https");
-var rest = require("rest");
 var fs = require("fs");
 
 var app = express();
-app.use(bodyParser.json());
 var port = 80; //3300 for local dev?
-
-
-
 
 //respond to all get requests
 app.get("/providers/:ukprn", (req, res) => handler(req,res));
 
+  function handler(req, res) {
 
-function handler(req, res) {
+    var ukprn = req.params["ukprn"];
+      
+    fs.readFile('data/provider.json', 'utf8', function (err,data) {
 
-  var ukprn = req.params["ukprn"];
+      data = data.replace(/@ukprn/g, ukprn);
 
-  
-  
-  fs.readFile('data/provider.json', 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      res.status(201).send(data);
 
-    data = data.replace(/@ukprn/g, ukprn);
-
-    //
-
-    if (err) {
-      return console.log(err);
-    }
-    res.status(201).send(data);
-
-  });
-
-
-  
-
-
-  //res.status(200).send(ukprn);
-  
+    });
 };
-
-
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
   });
-
-
-  
+ 
